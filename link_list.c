4510aec92e8a54a39b3dc29b0bafb83040ec9fff
Joel Lijo Mathew <joellijo4@gmail.com>
2025-10-20 16:52:28 +0000

    Added a second node to the linked list now two nodes

--- link_list.c
+++ link_list.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct node {
-    int data; 
-    struct node *link; 
+	int data; 
+	struct node *link; 
 }; 
-  
+
 int main(){
-     struct node *head = NULL; 
-     head = (struct node *)malloc(sizeof(struct node)); 
-     head->data = 45; 
-     head->link = NULL; 
-     printf("Data: %d", head->data); 
+	struct node *head = (struct node *)malloc(sizeof(struct node));
+	head->data = 45; 
+	head->link = NULL; 
+	printf("%d\n", head->data); 
+	struct node *current = (struct node *)malloc(sizeof(struct node)); 
+	head->link = current; 
+	current->data = 23; 
+	current->link = NULL; 
+	printf("Second: %d\n", current->data); 
+
+
+	return 0;
 }
