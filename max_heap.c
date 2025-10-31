0396d08d80b652bcab8b337b9fdc59aa492b59b9
Joel Lijo Mathew <joellijo4@gmail.com>
2025-10-31 10:18:54 +0000

    Max heap  - Insertion , Deletion Working completely fine need to implement heapify up/down later

--- max_heap.c
+++ max_heap.c
@@ -34,17 +34,46 @@ void insertion(Heap* a,int l, int value){
 	}
 }
 
+void deletion(Heap* a){
+	int l = a->size;
+	int i = 0;
+	int temp = a->arr[l-1];
+	a->arr[l-1] = a->arr[i];
+	a->arr[i] = temp; a->size = (a->size)-1; l--;int lc = 1, rc = 2;
+	while(lc <= l-1 || rc <= l-1 ){
+		int max;
+		if(a->arr[lc] > a->arr[rc]) max = lc;
+		else max = rc;
+
+		if(a->arr[i] < a->arr[max]){
+			temp = a->arr[max];
+			a->arr[max]= a->arr[i];
+			a->arr[i] = temp;
+			i = max;
+		}else return;
+		lc = (2*i)+1 , rc = lc+1;
+	}
+}
 
 int main(){
 	printf("Size: ");Heap* a = malloc(sizeof(Heap));
 	scanf("%d", &(a->size));a->capacity = 100;
-	printf("Elements: \n");
+	printf("Elements (in descending order - max heap): \n");
 	a->arr = malloc(sizeof(int)*a->size);
 	for(int i = 0 ; i < a->size; i++){
 		scanf("%d", &(a->arr[i]));
 	}
-	display(a);
-	printf("\nInsert: "); int v;scanf("%d", &v);
-	insertion(a,a->size,v);printf("\nInserted %d\n", v);
-	display(a);
+	while(1){
+		printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit\nChoice: ");
+		int choice; scanf("%d", &choice);
+		switch(choice){
+			case 1: printf("\nTo insert: ");int val;scanf("%d", &val);
+				insertion(a,a->size,val); printf("\nInserted: %d\n", val); 
+				break;
+			case 2: deletion(a);printf("\nDeleted Root: %d\n", a->arr[a->size]); 
+				break;
+			case 3: printf("\nMax Heap:\n");display(a);break;
+			default: return 0;
+		}
+	}
 }
